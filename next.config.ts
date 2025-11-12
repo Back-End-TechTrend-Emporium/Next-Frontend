45a90d20a988b3bc0cdeb90e00cd91a4d550f4aa
Carlos Pena <carlos.pena2@endava.com>
2025-11-12 16:05:13 -0500

    chore: initial import

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:7101";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      { protocol: "https", hostname: "fakestoreapi.com" },
+      { protocol: "https", hostname: "media.istockphoto.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "i.imgur.com" },
+      { protocol: "https", hostname: "www.bing.com" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+      { protocol: "https", hostname: "loremflickr.com" }
+    ]
+  },
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: `${BACKEND_URL}/api/:path*` }];
+  }
 };
 
 export default nextConfig;
